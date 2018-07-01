558e879f2f68721d017653818ce1de46a98fec5a
thezimtex <corey.scott.zimmerman@gmail.com>
2018-06-30 21:24:23 -0500

    Add files via upload

---
+++ scripts/linkedinmainbadge.js
@@ -0,0 +1,4 @@
+script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+
+<div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="zimmermancloud"><a class="LI-simple-link" href='https://www.linkedin.com/in/zimmermancloud?trk=profile-badge'>Corey Zimmerman</a></div>
+
---
+++ scripts/linkedinnamelink.js
@@ -0,0 +1,2 @@
+<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="IN/MemberProfile" data-id="https://www.linkedin.com/in/zimmermancloud" data-format="hover" data-related="false" data-text="Corey Zimmerman"></script>
